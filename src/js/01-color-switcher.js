76a0d3da818ba2f8659338166e59f04906917282
NikKms <mykola.kamyshnikov@gmail.com>
2023-05-03 13:45:39 +0300

    fix 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,17 +1,16 @@
 const refs = {
-  body: document.body,
   startBtn: document.querySelector('[data-start]'),
   stopBtn: document.querySelector('[data-stop]'),
 };
 
-refs.body.addEventListener('click', handlerBtn);
-refs.body.style.textAlign = 'center';
+refs.startBtn.addEventListener('click', startColorChange);
+refs.stopBtn.addEventListener('click', stopColorChange);
 
 let intervalID = null;
 
 function startColorChange() {
   intervalID = setInterval(() => {
-    refs.body.style.background = getRandomHexColor();
+    document.body.style.background = getRandomHexColor();
   }, 500);
 
   refs.startBtn.disabled = true;
@@ -23,11 +22,6 @@ function stopColorChange() {
 
   refs.startBtn.disabled = false;
   refs.stopBtn.disabled = true;
-}
-
-function handlerBtn({ target }) {
-  if (target === refs.startBtn) startColorChange();
-  else if (target === refs.stopBtn) stopColorChange();
 }
 
 function getRandomHexColor() {
