8bfd765d4c6abbbc27973441a738456d995cd1e4
NikKms <mykola.kamyshnikov@gmail.com>
2023-05-05 17:44:28 +0300

    upd timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,6 @@
 import flatpickr from 'flatpickr';
 import Notiflix from 'notiflix';
 import 'flatpickr/dist/flatpickr.min.css';
-import 'notiflix/dist/notiflix-3.2.6.min.css';
 
 const refs = {
   startButton: document.querySelector('[data-start]'),
